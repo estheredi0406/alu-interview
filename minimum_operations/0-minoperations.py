ec12974102d181067c1b5733937b03582a16a3a0
estheredi0406 <e.sessenou@alustudent.com>
2025-01-16 09:32:09 +0200

    module documented

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
+"""Minimum Operations"""
+import math
+
 
 def minOperations(n):
-    """Calculate the fewest number of operations needed to result in exactly n H characters."""
-    if n < 2:
+    """Minimum Operations"""
+    if n <= 1:
         return 0
-    
+
     operations = 0
-    factor = 2
-    
-    while n > 1:
-        while n % factor == 0:
-            operations += factor
-            n //= factor
-        factor += 1
-    
+    for i in range(2, int(math.sqrt(abs(n))) + 1):
+        while n % i == 0:
+            operations += i
+            n //= i
+
+    if n > 1:
+        operations += n
+
     return operations
